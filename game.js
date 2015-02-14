65aade4a884ea2478a3ab85bfc49db0d4a00bd39
Courtland Gaba <Courtland@Courtlands-Air.attlocal.net>
2015-02-14 05:00:51 -0500

    added hp bars

--- game.js
+++ game.js
@@ -8,9 +8,17 @@ function Player1(spec) {
     this.name = spec.name || "Player 1";
     this.life = 100;
     this.attack = function (player2) {
-        var hits = Math.floor(Math.random()*10);
+        var hits = Math.floor(Math.random()*20);
         console.log("hit");
         player2.damage(hits);
+    };
+    this.damage = function (hits) {
+        if(this.life >= 0) {
+            this.life = this.life - hits;
+            console.log(this.name + " was hit!!");
+        } else {
+            console.log(this.name + " HAS DIED, YOU WIN!!!!!");
+        };
     }
 }
 
@@ -19,16 +27,39 @@ function Player2(spec) {
     this.name = spec.name || "Player 2";
     this.life = 100;
     this.attack = function (player1) {
-        var hits = Math.floor(Math.random()*10);
+        var hits = Math.floor(Math.random()*20);
         console.log("hit");
         player1.damage(hits);
     }
+    this.damage = function (hits) {
+        if(this.life >= 0) {
+            this.life = this.life - hits;
+            console.log(this.name + " was hit!!");
+        } else {
+            console.log(this.name + " HAS DIED, YOU WIN!!!!!");
+        };
+    }
 }
+
+
+function score1 () {
+    this.score = 5;
+}
+
+function score2 () {
+    this.score = 5;
+}
+
+
+
+
+
+
+
 
 var fight = {
     init: function () {
         fight.initEvents();
-        fight.initStyling();
     },
     initStyling: function (){
 
@@ -39,18 +70,61 @@ var fight = {
             event.preventDefault();
             var value = $('#play1').val();
             $("#ply1").text(value);
+            console.log("hello");
+            var traits = {
+                name: $('#player1 input[name="name"]').val(),
+            };
+            fight.player1 = new Player1(traits);
+
         });
 /////Change player 2 name///////
         $("#player2").on('submit', function(event) {
             event.preventDefault();
             var value = $('#play2').val();
             $("#ply2").text(value);
+            console.log("hello");
+            var traits = {
+                name: $('#player2 input[name="name"]').val(),
+            };
+            fight.player2 = new Player2(traits);
+            $('.startFight').removeClass('hide');
+
+        });
+
+///////start game button/////
+        $(".startFight").on('click', function (event) {
+            event.preventDefault();
+            fight.renderBoard();
+            $('.startFight').addClass('hide');
+        });
+
+//////attack buttons/////
+        $("#goku").on("click", "#cat", function (e) {
+            e.preventDefault();
+            fight.player1.attack(fight.player2);
+            document.getElementById("pl2").value = fight.player2.life;
+
+            if (0 >= fight.player2.life) {
+            document.getElementById("www").text = fight.score1.score;
+
+            } else {
+
+            };
+        });
+
+
+        $("#frieza").on("click", "#dog", function (e) {
+            e.preventDefault();
+            fight.player2.attack(fight.player1);
+            document.getElementById("pl1").value = fight.player1.life;
         });
 
 
 
     },
     renderBoard: function () {
+        $("#goku").append("<button id='cat'>Attack</button>");
+        $("#frieza").prepend("<button id='dog'>Attack</button>");
 
     }
 };
