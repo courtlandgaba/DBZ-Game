5dcff79ff33111f10981c68eb3df24eed80dd34d
Courtland Gaba <Courtland@Courtlands-MacBook-Air.local>
2015-02-16 09:25:14 -0500

    clean up

--- game.js
+++ game.js
@@ -9,15 +9,12 @@ function Player1(spec) {
     this.score = 0;
     this.attack = function (player2) {
         var hits = Math.floor(Math.random()*20);
-        console.log("hit");
         player2.damage(hits);
     };
     this.damage = function (hits) {
         if(this.life >= 0) {
             this.life = this.life - hits;
-            console.log(this.name + " was hit!!");
         } else {
-            console.log(this.name + " HAS DIED, YOU WIN!!!!!");
         };
     }
 }
@@ -28,26 +25,21 @@ function Player2(spec) {
     this.score = 0;
     this.attack = function (player1) {
         var hits = Math.floor(Math.random()*20);
-        console.log("hit");
         player1.damage(hits);
     }
     this.damage = function (hits) {
         if(this.life >= 0) {
             this.life = this.life - hits;
-            console.log(this.name + " was hit!!");
         } else {
-            console.log(this.name + " HAS DIED, YOU WIN!!!!!");
         };
     }
 }
-
 
 var fight = {
     init: function () {
         fight.initEvents();
     },
     initStyling: function (){
-
     },
     initEvents: function () {
 /////Change player 1 name//////
@@ -55,27 +47,20 @@ var fight = {
             event.preventDefault();
             var value = $('#play1').val();
             $("#ply1").text(value);
-            console.log("hello");
             var traits = {
                 name: $('#player1 input[name="name"]').val(),
             };
             fight.player1 = new Player1(traits);
             if (value === ('cheese')) {
                 $("#bob").removeClass('hide');
-                console.log("kjkjhkjhkjh");
             } else {
-                console.log("nope");
             };
-
-
-
         });
 /////Change player 2 name///////
         $("#player2").on('submit', function(event) {
             event.preventDefault();
             var value = $('#play2').val();
             $("#ply2").text(value);
-            console.log("hello");
             var traits = {
                 name: $('#player2 input[name="name"]').val(),
             };
@@ -83,12 +68,9 @@ var fight = {
             $('.startFight').removeClass('hide');
             if (value === ('cheese')) {
                 $("#bob").removeClass('hide');
-                console.log("kjkjhkjhkjh");
             } else {
-                console.log("nope");
             }
         });
-
 ///////start game button/////
         $(".startFight").on('click', function (event) {
             event.preventDefault();
@@ -102,41 +84,31 @@ var fight = {
             fight.player2.life = 100;
             document.getElementById("pl1").value = fight.player1.life;
             document.getElementById("pl2").value = fight.player2.life;
-
         });
-
 //////attack buttons/////
         $("#goku").on("click", "#cat", function (e) {
             e.preventDefault();
             fight.player1.attack(fight.player2);
             document.getElementById("pl2").value = fight.player2.life;
-
             // $("#cat").addClass('hide');
             // $("#dog").removeClass('hide');
-
             if (0 >= fight.player2.life) {
                 $("#frieza").removeClass("frieza idle");
                 $("#frieza").addClass("deadfrieza");
                 $("#cat").addClass("hide");
                 $("#dog").addClass("hide");
                 $(".startFight").removeClass('hide');
-
                 fight.player1.score++;
                 document.getElementById("www").innerText = fight.player1.score;
-
-
             } else {
             }
         });
-
         $("#frieza").on("click", "#dog", function (e) {
             e.preventDefault();
             fight.player2.attack(fight.player1);
             document.getElementById("pl1").value = fight.player1.life;
-
             // $("#dog").addClass('hide');
             // $("#cat").removeClass('hide');
-
             if (0 >= fight.player1.life) {
                 $("#goku").removeClass("goku idle");
                 $("#goku").addClass("deadgoku");
@@ -147,8 +119,6 @@ var fight = {
                 document.getElementById("mmm").innerText = fight.player2.score;
             } else {
             }
-
-
         });
     },
     renderBoard: function () {
